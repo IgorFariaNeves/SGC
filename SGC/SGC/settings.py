782edd87e16e34bda2f472e7eda98e529fcad672
IGOR FARIA NEVES <igor.neves3@granbery.edu.dti>
2023-09-27 21:44:40 -0300

    update

--- SGC/SGC/settings.py
+++ SGC/SGC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$=6mjzakf=3$1#h-3g)whcjpi-m2$*ocbc)d6p(ipq5svf54*5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SGC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
