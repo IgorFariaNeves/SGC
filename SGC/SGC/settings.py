9ce43059c63f4997064e071cc338ada059d55c22
DANIEL VITOR DUARTE <daniel.duarte2@granbery.edu.dti>
2023-10-03 20:33:36 -0300

    atualizacao templates

--- SGC/SGC/settings.py
+++ SGC/SGC/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'aplic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'SGC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'BDSGC'),
+        'USER': os.environ.get('DB_USER', 'nome_do_user'),
+        'PASSWORD': os.environ.get('DB_PASS', 'senha_do_user'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
