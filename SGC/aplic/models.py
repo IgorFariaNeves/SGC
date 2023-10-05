68472805867c0acca4f89c31749e24f5c386370b
DANIEL VITOR DUARTE <daniel.duarte2@granbery.edu.dti>
2023-10-04 22:06:12 -0300

    atualizacao classes

--- SGC/aplic/models.py
+++ SGC/aplic/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50, verbose_name="Qual seu nome?", help_text="Digite seu nome completo")
+    email = models.CharField(max_length=100, verbose_name="Digite seu email")
+    def __str__(self):
+        return self.nome
+
+class Telefone(models.Model):
+    numero = models.CharField(max_length=20, verbose_name="Digite seu telefone")
+
+#class Endereco(models.Model):
+#   logradouro = models.CharField(max_length=50, verbose_name=)
