42053a95309b9e3360423ff18a1829cb95c44bf0
DANIEL VITOR DUARTE <daniel.duarte2@granbery.edu.dti>
2023-11-21 20:27:09 -0300

    atualizacao descricao

--- SGC/aplic/urls.py
+++ SGC/aplic/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, home, FuncionarioDetalheView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("", home, name='home'),
+    path('funcionario-detalhe/<int:id>/', FuncionarioDetalheView.as_view(), name='funcionario-detalhe'),
 ]
--- SGC/aplic/views.py
+++ SGC/aplic/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
+from .models import Funcionario
 from django.views.generic import TemplateView
-from aplic.models import Funcionario
-
+from django.views.generic import ListView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -10,3 +11,27 @@ class IndexView(TemplateView):
         context['funcionarios'] = Funcionario.objects.all()
         return context
 
+def home(request):
+    query = request.GET.get('q')
+    funcionarios = Funcionario.objects.all()
+
+    if query:
+        funcionarios = funcionarios.filter(nome__icontains=query)
+
+    return render(request, 'index.html', {'funcionarios': funcionarios})
+
+class FuncionarioDetalheView(ListView):
+    template_name = 'funcionario-detail.html'
+    paginate_by = 5
+    ordering = 'nome'
+    model = Funcionario
+
+    def get_context_data(self, **kwargs):
+        context = super(FuncionarioDetalheView, self).get_context_data(**kwargs)
+        id = self.kwargs['id']
+        context['funcionario'] = Funcionario.objects.filter(id=id).first
+        return context
+
+    def get_queryset(self, **kwargs):
+        id = self.kwargs['id']
+        return Funcionario.objects.filter(funcionario_id=id)
