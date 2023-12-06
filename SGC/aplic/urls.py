da45c165444a60603ba66ef457f373eaefb26841
Daniel Duarte <danielvitasso@gmail.com>
2023-12-05 22:51:23 -0300

    atualizacao

--- SGC/aplic/urls.py
+++ SGC/aplic/urls.py
@@ -3,6 +3,7 @@ from .views import IndexView, home, FuncionarioDetalheView, detalhes_funcionario
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("home/", home, name='home'),
@@ -12,3 +13,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
